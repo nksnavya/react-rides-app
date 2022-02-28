a993c8c9b22a82cac527fbfc4dda317130abe752
Navya KS <nksnavya@gmail.com>
2022-02-28 20:54:02 +0530

    rides app

---
+++ src/components/Home.js
@@ -0,0 +1,142 @@
+import { useEffect, useState } from "react";
+import {
+  Container,
+  Navbar,
+  Image,
+  Tabs,
+  Tab,
+  Button,
+  OverlayTrigger,
+  Popover,
+  Form,
+} from "react-bootstrap";
+import Rides from "./Rides";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  const [cityList, setCityList] = useState([]);
+  const [stateList, setStateList] = useState([]);
+  const [user, setUser] = useState({});
+  const [ridesList, setRidesList] = useState([]);
+  const [originalRides, setOriginalRides] = useState([]);
+  const [filterList, setFilterList] = useState({
+    state: "",
+    city: "",
+  });
+
+  useEffect(() => {
+    function fetchData() {
+      // Note: getting CORS error for API's so reading the data from json files
+      // const userRes = await fetch('https://assessment.api.vweb.app/user');
+      // const userInfo =  userRes.json()
+      const ridesRes = require("../data/rides.json");
+      const userRes = require("../data/user.json");
+      const states = ridesRes.map((rider) => rider.state);
+      const statesList = Array.from(new Set(states));
+      setStateList(statesList);
+      setUser(userRes);
+      setRidesList(ridesRes);
+      setOriginalRides(ridesRes);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (ridesList.length > 0) {
+      let filteredList = [...originalRides];
+      if (filterList.state) {
+        filteredList = filteredList.filter(
+          (ride) => ride.state === filterList.state
+        );
+      }
+      if (filterList.city) {
+        filteredList = filteredList.filter(
+          (ride) => ride.city === filterList.city
+        );
+      }
+      setRidesList(filteredList);
+    }
+  }, [filterList]);
+
+  const handleStateChange = (e) => {
+    const selectedState = e.target.value;
+    console.log(selectedState);
+    setFilterList({ ...filterList, state: selectedState, city: "" });
+    if (selectedState) {
+      const filteredCityList = originalRides.filter(
+        (ride) => ride.state === selectedState
+      );
+      setCityList(filteredCityList);
+    }
+  };
+
+  const handleCityChange = (e) => {
+    const selectedCity = e.target.value;
+    console.log(selectedCity);
+    setFilterList({ ...filterList, city: selectedCity });
+  };
+
+  const ridesFilter = (
+    <Popover id="filter-menu">
+      <Popover.Header as="h3">Filters</Popover.Header>
+      <Popover.Body>
+        <Form.Select value={filterList.state} onChange={handleStateChange}>
+          <option value="">Select State</option>
+          {stateList.map((state, i) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
+          ))}
+        </Form.Select>
+        <Form.Select
+          value={filterList.city}
+          onChange={handleCityChange}
+          className="mt-2"
+        >
+          <option value="">Select City</option>
+          {cityList.map((ride, i) => (
+            <option key={`city+${i}`} value={ride.city}>
+              {ride.city}
+            </option>
+          ))}
+        </Form.Select>
+      </Popover.Body>
+    </Popover>
+  );
+
+  return (
+    <main className="main-container">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Edvora</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>{user.name}</Navbar.Text>
+            <Image
+              fluid
+              roundedCircle
+              className={`${styles.userIcon} ms-3`}
+              src={user.url}
+            />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <OverlayTrigger trigger="click" placement="bottom" overlay={ridesFilter}>
+        <Button variant="secondary" className={styles.filterButton}>
+          <Image src={require("../assets/filter.png")} fluid className="pe-2" />
+          Filter
+        </Button>
+      </OverlayTrigger>
+      <Tabs defaultActiveKey="nearest" id="rides-tab" className="mb-3">
+        <Tab eventKey="nearest" title="Nearest rides">
+          <Rides rides={ridesList} />
+        </Tab>
+        <Tab eventKey="upcoming" title="upcoming rides">
+          <Rides rides={ridesList} rideType="upcoming" />
+        </Tab>
+        <Tab eventKey="past" title="past rides">
+          <Rides rides={ridesList} rideType="past" />
+        </Tab>
+      </Tabs>
+    </main>
+  );
+}
---
+++ src/components/Rides.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { Image } from "react-bootstrap";
+import styles from "./Rides.module.css";
+
+const InfoText = ({ label, value }) => {
+  return (
+    <p className={`${styles.infoTextLabel} ms-4 mb-0`}>
+      {label}
+      <span className="ps-1 text-white">{value}</span>
+    </p>
+  );
+};
+
+const Rides = ({ rides, rideType }) => {
+  const [rideList, setRideList] = useState([]);
+
+  useEffect(() => {
+    let filteredRides = [...rides];
+    if (rideType === "upcoming") {
+      filteredRides = rides.filter((ride) => new Date(ride.date) > Date.now());
+    } else if (rideType === "past") {
+      filteredRides = rides.filter((ride) => new Date(ride.date) < Date.now());
+    }
+    setRideList(filteredRides);
+  }, [rides]);
+
+  return (
+    <>
+      {rideList.length === 0 && <p>No Rides Available</p>}
+      {rideList.map((ride, i) => (
+        <div key={ride.date} className={`${styles.rideList} ms-4 mb-2`}>
+          <Image
+            alt="loaction"
+            style={{ height: 100 }}
+            src={require("../assets/location.png")}
+            thumbnail
+          />
+          <div className="ms-4">
+            <InfoText label="Ride Id:" value={ride.id} />
+            <InfoText
+              label="Origin station:"
+              value={ride.origin_station_code}
+            />
+            <InfoText label="Station_path:" value={ride.station_path} />
+            <InfoText label="Date:" value={ride.date} />
+            <InfoText label="Distance:" value={i} />
+          </div>
+          <div className={styles.placeWrapper}>
+            <span className={`${styles.placeName} py-1 px-2 mx-2`}>
+              {ride.city}
+            </span>
+            <span className={`${styles.placeName} py-1 px-2 mx-2`}>
+              {ride.state}
+            </span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Rides;
